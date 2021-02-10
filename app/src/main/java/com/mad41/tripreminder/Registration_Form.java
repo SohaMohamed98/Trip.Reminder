67bfa50b2186876830237db08804685b3f4fd4bf
MarwaEbrahem <31275340+MarwaEbrahem@users.noreply.github.com>
2021-02-10 15:10:39 +0200

    auth design

--- app/src/main/java/com/mad41/tripreminder/Registration_Form.java
+++ app/src/main/java/com/mad41/tripreminder/Registration_Form.java
@@ -1,6 +1,8 @@
 package com.mad41.tripreminder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -22,12 +24,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.mad41.tripreminder.Firebase.checkConnectionToInternet;
 
 public class Registration_Form extends AppCompatActivity {
+
     EditText firstName, email,password,confirmationPassword;
     FirebaseAuth firebaseAuth;
     ProgressBar progressBar;
-    Button Signup ;
+    Button Signup , Login ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,7 @@ public class Registration_Form extends AppCompatActivity {
         email = (EditText)findViewById(R.id.E_mail);
         password = (EditText)findViewById(R.id.passwordID);
         Signup = findViewById(R.id.signUp);
+        Login = findViewById(R.id.Login);
         confirmationPassword = (EditText)findViewById(R.id.ConfirmPasswordID);
         progressBar = findViewById(R.id.signUPprogressBar);
         firebaseAuth = FirebaseAuth.getInstance();
@@ -46,23 +52,18 @@ public class Registration_Form extends AppCompatActivity {
                 signUp();
             }
         });
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Main = new Intent(getApplicationContext(), Login_form.class);
+                startActivity(Main);
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-    }
-   private void UpdateUi(FirebaseUser firebaseUser) {
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
-        if(account != null)
-        {
-            String personName = account.getDisplayName();
-            String personGivenName = account.getGivenName();
-            String personEmail = account.getEmail();
-            Uri personPhoto = account.getPhotoUrl();
-            Toast.makeText(this, personName +","+personEmail, Toast.LENGTH_SHORT).show();
-        }
 
     }
 
@@ -77,58 +78,74 @@ public class Registration_Form extends AppCompatActivity {
             firstName.requestFocus();
 
         }
-        if(TextUtils.isEmpty(emailString) && ! Patterns.EMAIL_ADDRESS.matcher(emailString).matches())
+       else if(TextUtils.isEmpty(emailString) || ! Patterns.EMAIL_ADDRESS.matcher(emailString).matches())
         {
             email.setError("Enter Valid Email.");
             email.requestFocus();
 
         }
-       if(TextUtils.isEmpty(passString))
+       else if(TextUtils.isEmpty(passString))
         {
             password.setError("Password is Required.");
             password.requestFocus();
 
         }
-        if(TextUtils.isEmpty(confPassString))
+       else if(TextUtils.isEmpty(confPassString))
         {
             password.setError("confirm Password is Required.");
             password.requestFocus();
 
         }
-        if(passString.length() < 6)
+        else if(passString.length() < 6)
         {
             password.setError("Password must be more 6 digits");
             password.requestFocus();
 
         }
-        if(!passString.equals(confPassString))
+       else if(!passString.equals(confPassString))
         {
             Toast.makeText(this, "Password not matching", Toast.LENGTH_SHORT).show();
             confirmationPassword.setError("Password must be more 6 digits");
             confirmationPassword.requestFocus();
 
         }
-        if(passString.equals(confPassString) && !passString.isEmpty()) {
+       else if(passString.equals(confPassString) && !passString.isEmpty()) {
             progressBar.setVisibility(View.VISIBLE );
+            if(checkConnectionToInternet.isConnected(getApplicationContext())) {
+                firebaseAuth.createUserWithEmailAndPassword(emailString, passString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if ((task.isSuccessful())) {
+                            Toast.makeText(Registration_Form.this, "Registeration Done", Toast.LENGTH_SHORT).show();
+                            Login_form.readFireBase.start();
+                            Intent Main = new Intent(getApplicationContext(), MainScreen.class);
+                            startActivity(Main);
+                            firebaseAuth = FirebaseAuth.getInstance();
+                            writeInSharedPreference();
 
-            firebaseAuth.createUserWithEmailAndPassword(emailString, passString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if ((task.isSuccessful())) {
-                        Toast.makeText(Registration_Form.this, "Registeration Done", Toast.LENGTH_SHORT).show();
-                        Login_form.readFireBase.start();
-                        Intent Main = new Intent(getApplicationContext(), MainScreen.class);
-                        startActivity(Main);
+                        } else {
+                            Toast.makeText(Registration_Form.this, "Faild !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.INVISIBLE );
+                        }
 
-                    } else {
-                        Toast.makeText(Registration_Form.this, "Error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
-
-                }
-            });
+                });
+            }else{
+                Toast.makeText(Registration_Form.this, "Please, check your internet connection " , Toast.LENGTH_LONG).show();
+                progressBar.setVisibility(View.INVISIBLE );
+            }
         }
 
     }
 
-
+    public void writeInSharedPreference(){
+        SharedPreferences writr = getSharedPreferences(Login_form.PREFS_NAME , Context.MODE_PRIVATE);
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        SharedPreferences.Editor editor = writr.edit();
+        editor.putString("UserName",user.getDisplayName());
+        editor.putString("Email",user.getEmail());
+        editor.commit();
+        System.out.println("user name is : "+user.getDisplayName());
+        System.out.println("email is : "+user.getEmail());
+    }
 }
